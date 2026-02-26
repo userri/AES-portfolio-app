cfc189016295abd3342f1d6173072a92a0c683cb
임유리 <imyuri@im-yuliui-iMac.local>
2026-02-26 17:47:56 +0900

    ProjectCard 인원수 태그 색 수정

--- src/components/ui/ProjectCard.tsx
+++ src/components/ui/ProjectCard.tsx
@@ -1,18 +1,32 @@
 import SkillTag from "./SkillTag";
 import styles from "./style/ProjectCard.module.css";
-import iconImg from "../../assets/image/black-link.webp";
+import iconImgSample from "../../assets/image/black-link.webp";
 
-const ProjectCard = () => {
+interface ProjectCardProps {
+  iconImg?: string;
+  memberCnt?: number;
+  title: string;
+  serviceLink?: string;
+  skills?: string[];
+}
+
+const ProjectCard = ({
+  iconImg = iconImgSample,
+  memberCnt = 1,
+  title,
+  serviceLink = "https://github.com/userri/",
+  skills = ["Java", "JavaScript", "Spring Boot", "JPA", "MySQL", "Redis"],
+}: ProjectCardProps) => {
   return (
     <div className={styles.body}>
       <div className={styles.top}>
         <img className={styles.icon} src={iconImg} alt="아이콘" />
-        <span className={styles.memberCount}>3인 프로젝트</span>
+        <span className={styles.memberCount}>{memberCnt}인 프로젝트</span>
       </div>
-      <div className={styles.title}>프로젝트 제목</div>
+      <div className={styles.title}>{title}</div>
       <div className={styles.serviceLink}>
         <img src={iconImg} alt="" />
-        <div className={styles.serviceLink}>프로젝트 실제 서비스 링크</div>
+        <div className={styles.serviceLink}>{serviceLink}</div>
       </div>
       <div className={styles.line}></div>
       <div className={styles.subtitle}>프로젝트 한줄소개</div>
@@ -23,9 +37,9 @@ const ProjectCard = () => {
       <div className={styles.line}></div>
       <div></div>
       <div className={styles.skills}>
-        <SkillTag text="Java" bgColor="black" textColor="white" />
-        <SkillTag text="JavaScript" bgColor="black" textColor="white" />
-        <SkillTag text="PortgreSQL" bgColor="black" textColor="white" />
+        {skills.map((skill) => (
+          <SkillTag text={skill} bgColor="black" textColor="white" />
+        ))}
       </div>{" "}
       <div className={styles.duration}>2026-02-24 ~ 2026-03-04</div>
     </div>
