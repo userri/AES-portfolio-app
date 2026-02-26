dfd862571ffd380316d6467afc0115bb28db0dd6
임유리 <imyuri@im-yuliui-iMac.local>
2026-02-26 10:07:22 +0900

    ProjectCard 컴포넌트 구현

--- src/components/ui/ProjectCard.tsx
+++ src/components/ui/ProjectCard.tsx
@@ -1,23 +1,24 @@
 import SkillTag from "./SkillTag";
 import styles from "./style/ProjectCard.module.css";
+import iconImg from "../../assets/image/black-link.webp";
 
 const ProjectCard = () => {
   return (
     <div className={styles.body}>
       <div className={styles.top}>
-        <img className={styles.icon} src="" alt="아이콘" />
+        <img className={styles.icon} src={iconImg} alt="아이콘" />
         <span className={styles.memberCount}>3인 프로젝트</span>
       </div>
       <div className={styles.title}>프로젝트 제목</div>
       <div className={styles.serviceLink}>
-        <img src="" alt="" />
+        <img src={iconImg} alt="" />
         <div className={styles.serviceLink}>프로젝트 실제 서비스 링크</div>
       </div>
       <div className={styles.line}></div>
       <div className={styles.subtitle}>프로젝트 한줄소개</div>
-      <div className={styles.description}>프로젝트 설명 1</div>
-      <div className={styles.description}>프로젝트 설명 2</div>
-      <div className={styles.description}>프로젝트 설명 3</div>
+      <div className={styles.description}>- 프로젝트 설명 1</div>
+      <div className={styles.description}>- 프로젝트 설명 2</div>
+      <div className={styles.description}>- 프로젝트 설명 3</div>
       <div className={styles.subtitle}>기술스택</div>
       <div className={styles.line}></div>
       <div></div>
