ea6907bcbe628b08d3b96575987a759028d4a47e
임유리 <imyuri@im-yuliui-iMac.local>
2026-02-26 17:49:23 +0900

    Archiving 구현

---
+++ src/pages/home/sections/Archiving.tsx
@@ -0,0 +1,29 @@
+import SectionTitle from "../../../components/ui/SectionTitle";
+import styles from "../../style/Archiving.module.css";
+import githubImg from "../../../assets/image/github.png";
+
+const Archiving = () => {
+  const data = [];
+  return (
+    <div className={styles.body}>
+      <SectionTitle title="ARCHIVING" />
+      <div className={styles.boxes}>
+        <div className={styles.box}>
+          <img className={styles.img} src={githubImg} alt="" />
+          <a className={styles.link} href="https://github.com/userri">
+            https://github.com/userri
+          </a>
+          <div className={styles.description}>소스 코드 저장소</div>
+        </div>
+        <div className={styles.box}>
+          <img className={styles.img} src={githubImg} alt="" />
+          <a className={styles.link} href="https://github.com/userri">
+            https://github.com/userri
+          </a>
+          <div className={styles.description}>소스 코드 저장소</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Archiving;
