c11014a6d2338ec3f02126907761ef35a9951634
임유리 <imyuri@im-yuliui-iMac.local>
2026-02-26 15:07:17 +0900

    Skills 구현

---
+++ src/pages/home/sections/Skills.tsx
@@ -0,0 +1,134 @@
+import SectionTitle from "../../../components/ui/SectionTitle";
+import styles from "../../style/Skills.module.css";
+import sampleImg from "../../../assets/image/calendar.png";
+import SkillTag from "../../../components/ui/SkillTag";
+const Skills = () => {
+  const getRandomColor = () =>
+    `#${Math.floor(Math.random() * 0xffffff)
+      .toString(16)
+      .padStart(6, "0")}`;
+
+  const skills = [
+    { skillName: "JavaScript", skillColor: getRandomColor() },
+    { skillName: "Java", skillColor: getRandomColor() },
+    { skillName: "Docker", skillColor: getRandomColor() },
+  ];
+  return (
+    <div className={styles.body}>
+      <SectionTitle title="SKILLS" />
+      <div className={styles.box}>
+        <div className={styles.skillBox}>
+          {/* TODO: map으로 넣기 */}
+          <img src={sampleImg} alt="" />
+          <div className={styles.category}>Language</div>
+          <div className={styles.skillContents}>
+            <SkillTag
+              text="Typescript"
+              bgColor={getRandomColor()}
+              textColor={getRandomColor()}
+            />
+            <SkillTag
+              text="JavaScript"
+              bgColor={getRandomColor()}
+              textColor={getRandomColor()}
+            />
+            <SkillTag
+              text="Python"
+              bgColor={getRandomColor()}
+              textColor={getRandomColor()}
+            />
+            <SkillTag
+              text="Kotlin"
+              bgColor={getRandomColor()}
+              textColor={getRandomColor()}
+            />
+          </div>
+        </div>
+
+        <div className={styles.skillBox}>
+          {/* TODO: map으로 넣기 */}
+          <img src={sampleImg} alt="" />
+          <div className={styles.category}>Frontend</div>
+          <div className={styles.skillContents}>
+            <SkillTag
+              text="Next.js / React"
+              bgColor={getRandomColor()}
+              textColor={getRandomColor()}
+            />
+            <SkillTag
+              text="React-Query"
+              bgColor={getRandomColor()}
+              textColor={getRandomColor()}
+            />
+            <SkillTag
+              text="Vite"
+              bgColor={getRandomColor()}
+              textColor={getRandomColor()}
+            />
+          </div>
+        </div>
+        <div className={styles.skillBox}>
+          {/* TODO: map으로 넣기 */}
+          <img src={sampleImg} alt="" />
+          <div className={styles.category}>Backend</div>
+          <div className={styles.skillContents}>
+            <SkillTag
+              text="Spring (Boot)"
+              bgColor={getRandomColor()}
+              textColor={getRandomColor()}
+            />
+            <SkillTag
+              text="Gradle"
+              bgColor={getRandomColor()}
+              textColor={getRandomColor()}
+            />
+            <SkillTag
+              text="Firebase"
+              bgColor={getRandomColor()}
+              textColor={getRandomColor()}
+            />
+            <SkillTag
+              text="Supabase"
+              bgColor={getRandomColor()}
+              textColor={getRandomColor()}
+            />
+          </div>
+        </div>
+
+        <div className={styles.skillBox}>
+          {/* TODO: map으로 넣기 */}
+          <img src={sampleImg} alt="" />
+          <div className={styles.category}>DevOps</div>
+          <div className={styles.skillContents}>
+            <SkillTag
+              text="Docker"
+              bgColor={getRandomColor()}
+              textColor={getRandomColor()}
+            />
+            <SkillTag
+              text="AWS (ECS, EB)"
+              bgColor={getRandomColor()}
+              textColor={getRandomColor()}
+            />
+            <SkillTag
+              text="Kubernetes"
+              bgColor={getRandomColor()}
+              textColor={getRandomColor()}
+            />
+            <SkillTag
+              text="Redis"
+              bgColor={getRandomColor()}
+              textColor={getRandomColor()}
+            />
+            <SkillTag
+              text="Vercel"
+              bgColor={getRandomColor()}
+              textColor={getRandomColor()}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Skills;
