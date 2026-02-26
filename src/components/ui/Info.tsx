988d559c672b9e5e6b599fe6945eb542d72909ed
임유리 <imyuri@im-yuliui-iMac.local>
2026-02-26 11:31:56 +0900

    About.tsx 내부의Info 컴포넌트 구현

---
+++ src/components/ui/Info.tsx
@@ -0,0 +1,26 @@
+import img from "../../assets/image/person.png";
+import styles from "../ui/style/Info.module.css";
+
+interface InfoProps {
+  imgSrc: string;
+  category: string;
+  content: string;
+}
+
+const Info = ({
+  imgSrc = img,
+  category = "이름",
+  content = "홍길동",
+}: InfoProps) => {
+  return (
+    <div className={styles.body}>
+      <img className={styles.img} src={imgSrc} alt="" />
+      <div className={styles.text}>
+        <div className={styles.category}>{category}</div>
+        <div className={styles.content}>{content}</div>
+      </div>
+    </div>
+  );
+};
+
+export default Info;
