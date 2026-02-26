2bbd6cdb9e362bac52449bcca09b50bdbe480c62
임유리 <imyuri@im-yuliui-iMac.local>
2026-02-26 17:43:21 +0900

    Career 기본 골격 구현

---
+++ src/pages/home/sections/Career.tsx
@@ -0,0 +1,11 @@
+import SectionTitle from "../../../components/ui/SectionTitle";
+import styles from "../../style/Career.module.css";
+const Career = () => {
+  const data = [];
+  return (
+    <div className={styles.body}>
+      <SectionTitle title="CAREER" />
+    </div>
+  );
+};
+export default Career;
