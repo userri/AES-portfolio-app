5fae986e44b8948ac8a748fc2fd998492647361d
임유리 <imyuri@im-yuliui-iMac.local>
2026-02-26 15:58:34 +0900

    SectionTitle 여백 수정

--- src/components/ui/SectionTitle.tsx
+++ src/components/ui/SectionTitle.tsx
@@ -2,20 +2,26 @@ import styles from "./style/SectionTitle.module.css";
 import chainImage from "../../assets/image/black-link.webp";
 
 interface SectionTitleProps {
+  img?: string;
   title: string;
 }
 
-const SectionTitle = ({ title }: SectionTitleProps) => {
+const SectionTitle = ({ img = chainImage, title }: SectionTitleProps) => {
   return (
     <div className={styles.container}>
-      <img className={styles.img} src={chainImage} alt="링크 이미지" />
+      <img className={styles.img} src={img} alt="링크 이미지" />
 
       <div className={styles.titleWrapper}>
         <h1 className={styles.title}>{title}</h1>
         <div className={styles.line} />
       </div>
+      <div className={styles.blank}></div>
     </div>
   );
+};
+SectionTitle.defaultProps = {
+  img: chainImage,
+  title: "SectionTitle",
 };
 
 export default SectionTitle;
