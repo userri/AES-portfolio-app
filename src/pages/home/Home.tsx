61bd8c1501ec0eac09015a03dfce926c23e8f3dc
임유리 <imyuri@im-yuliui-iMac.local>
2026-02-26 17:40:27 +0900

    테스트 페이지

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -2,6 +2,18 @@ import { Link } from "react-router-dom";
 import SectionTitle from "../../components/ui/SectionTitle";
 import SkillTag from "../../components/ui/SkillTag";
 import ProjectCard from "../../components/ui/ProjectCard";
+import Info from "../../components/ui/Info";
+import person from "../../assets/image/person.png";
+import calendar from "../../assets/image/calendar.png";
+import location from "../../assets/image/location.png";
+import call from "../../assets/image/call.png";
+import mail from "../../assets/image/mail.png";
+import pencil from "../../assets/image/pencil.png";
+import About from "./sections/About";
+import Skills from "./sections/Skills";
+import Archiving from "./sections/Archiving";
+import Projects from "./sections/Projects";
+import Career from "./sections/Career";
 
 const Home = () => {
   return (
@@ -21,25 +33,32 @@ const Home = () => {
       <br />
       <br />
       <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-
       <ProjectCard />
       <br />
       <br />
       <br />
+      <Info imgSrc={person} category="이름" content="임유리" />
+      <br />
+      <Info imgSrc={calendar} category="생년월일" content="01.06.07" />
+      <br />
+      <Info imgSrc={location} category="위치" content="가산동" />
+      <br />
+      <Info imgSrc={call} category="연락처" content="010-1234-5678" />
+      <br />
+      <Info imgSrc={mail} category="이메일" content="dladbfl67@example.com" />
+      <br />
+      <Info
+        imgSrc={pencil}
+        category="학력"
+        content="오토에버대학교(백엔드학과)"
+      />
       <br />
       <br />
+      <About />
+      <Skills />
+      <Archiving />
+      <Projects />
+      <Career />
       <br />
       <br />
       <br />
