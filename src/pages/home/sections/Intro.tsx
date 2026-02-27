722f44bfccf6a995891d2667d87bdd0ba54b85a3
임유리 <imyuri@im-yuliui-iMac.local>
2026-02-27 15:20:03 +0900

    Header -> Intro 파일 내용 이동

---
+++ src/pages/home/sections/Intro.tsx
@@ -0,0 +1,25 @@
+import styles from "../../style/Intro.module.css";
+const Header = () => {
+  return (
+    <div className={styles.body}>
+      <div className={styles.header}>
+        <div className={styles.title}>IYR's Portfolio</div>
+        <div className={styles.nav}>
+          <div>About me</div>
+          <div>Skills</div>
+          <div>Archiving</div>
+          <div>Projects</div>
+          <div>Career</div>
+        </div>
+      </div>
+      <div className={styles.name}>-임유리-</div>
+      <div className={styles.name}>백엔드 개발자 포트폴리오</div>
+      <div className={styles.line}></div>
+      <div className={styles.intro}>안녕하세요.</div>
+      <div className={styles.intro}>본질에 충실한 백엔드 개발자</div>
+      <div className={styles.intro}>임유리입니다.</div>
+      <div className={styles.more}>더 알아보기 ↓</div>
+    </div>
+  );
+};
+export default Header;
